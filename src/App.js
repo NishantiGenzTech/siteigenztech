a0f1a24e1b66926fd8f582ada543727e336a75b8
NishantiGenzTech <Nishant.Kumar@igenztech.com>
2021-04-07 12:47:10 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React ,{ useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Product from './components/pages/Product';
+import Careers from './components/pages/Careers';
+import ContactUs from './components/pages/ContactUs';
+// import JsonData from './data/data.json'
+import StaffingSolutions from './components/pages/StaffingSolutions';
+import AIHiring from './components/pages/AIHiring';
 
 function App() {
+  // const [landingPageData, setLandingPageData] = useState({})
+  // useEffect(() => {
+  //   setLandingPageData(JsonData)
+  // }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/StaffingSolutions' component={StaffingSolutions} />
+          <Route path='/Careers' component={Careers} />
+          <Route path='/contact-us' component={ContactUs} />
+          <Route path='/StaffingSolutions' component={StaffingSolutions} />
+          <Route path='/AIHiring' component={AIHiring} />
+        </Switch>
+      </Router>
     </div>
   );
 }
